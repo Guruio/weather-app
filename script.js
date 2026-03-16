ce4680a5dd4cd3bc0308cb73f8546c09db32d612
Guruprakash <p.guruprakash123456789@gmail.com>
2026-03-16 15:18:35 +0000

    fix weather app

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const apiKey = "YOUR_API_KEY"
+const apiKey = "fef2cb2811ed40ee5e6af071856c9ee7"
 
-async function getWeather(){
+async function getWeather() {
 
 const city = document.getElementById("city").value
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+const url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 const response = await fetch(url)
 
@@ -13,15 +14,31 @@ const data = await response.json()
 const result = document.getElementById("weatherResult")
 
 if(data.cod != 200){
-    result.innerHTML = "City not found or API error"
-    return
+result.innerHTML = "City not found"
+return
 }
+
+const icon = data.weather[0].icon
+const iconUrl =
+`https://openweathermap.org/img/wn/${icon}@2x.png`
 
 result.innerHTML = `
 <h2>${data.name}</h2>
-<p>Temperature: ${data.main.temp}°C</p>
+<img src="${iconUrl}">
+<p>Temperature: ${Math.round(data.main.temp)}°C</p>
 <p>Humidity: ${data.main.humidity}%</p>
 <p>Weather: ${data.weather[0].main}</p>
 `
-
 }
+
+/* ENTER KEY SEARCH */
+
+document
+.getElementById("city")
+.addEventListener("keydown", function(event){
+
+if(event.key === "Enter"){
+getWeather()
+}
+
+})
